d1cc1485e825824736b523ec668894019daca3f9
Sebastian Sanchez <sslsebas@gmail.com>
2020-12-21 10:50:55 -0500

    creacion de prueba de menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalidadComponent } from './calidad/calidad.component';
+import { CrearComponent } from './crear/crear.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmpresasComponent } from './empresas/empresas.component';
+import { ListarComponent } from './listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: DashboardComponent
+  },
+  {
+    path:"empresas",
+    component:EmpresasComponent
+  },
+  {
+    path:"crear",
+    component:CrearComponent
+  },
+  {
+    path:"listar",
+    component:ListarComponent
+  },
+  {
+    path:"calidad",
+    component:CalidadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarModule } from 'ng-sidebar';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmpresasComponent } from './empresas/empresas.component';
+import { ListarComponent } from './listar/listar.component';
+import { CrearComponent } from './crear/crear.component';
+import { CalidadComponent } from './calidad/calidad.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EmpresasComponent,
+    ListarComponent,
+    CrearComponent,
+    CalidadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
