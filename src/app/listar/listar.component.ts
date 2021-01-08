aa198243436867a87a738d2482e312a06226ed32
Sebastian Sanchez <sslsebas@gmail.com>
2021-01-08 17:03:37 -0500

    tenemos que depurar! cuadre el sidebar de mejor manera.

--- src/app/listar/listar.component.ts
+++ src/app/listar/listar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar',
   templateUrl: './listar.component.html',
-  // styleUrls: ['./listar.component.css','../empresas/empresas.component.css']
   styleUrls: ['./listar.component.css']
 })
 export class ListarComponent implements OnInit {
